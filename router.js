4a4042a173a85a49a193b1384aa8f144dca3e4f4
Robert Benavidez <murabitbenavidez@gmail.com>
2019-10-02 10:00:35 -0400

    all endpoints

--- router.js
+++ router.js
@@ -25,7 +25,7 @@ router.get('/:id', (req, res) => {
     const {id} = req.params
     Posts.findById(id)
         .then(([post]) => {
-            console.log(post)
+        
             if (!post) {
                 res.status(404).json({message: "ID does not exist"})
             } else {
@@ -37,6 +37,89 @@ router.get('/:id', (req, res) => {
         })
 })
 
+// GET  /api/posts/:id/comments 
+ //Returns an array of all the comment objects 
+ //associated with the post with the specified id.
+
+ router.get('/:id/comments', (req, res) => {
+     const postId = req.params.id;
+
+
+     Posts.findPostComments(postId)
+        .then(comment => {
+        if (comment.length > 0) {
+            res.status(200).json(comment);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist" });
+        }
+        })
+        .catch(error => {
+        res.status(500).json({ message: "The comments information could not be retrieved" });
+        });
+ })
+
+
+// POST new POST
+
+
+router.post("/", (req, res) => {
+    const newPost = req.body;
+    if (newPost.title && newPost.contents) {
+      Posts.insert(newPost)
+        .then(post => {
+          res.status(201).json(newPost);
+        })
+        .catch(error => {
+          res.status(500).json({ message: "Unable to add post" });
+        });
+    } else {
+      res.status(400).json({ message: "Title and contents required."});
+    }
+  });
+
+  router.post("/:id/comments", (req, res) => {
+    const id = req.params.id;
+    const post = {
+        "post_id": id,
+        "text": req.body.text
+    }
+  Posts.insertComment(post)
+  .then(newPost => {
+    res.status(201).json(newPost)
+  })
+  .catch(error => {
+    res.status(500).json({message:error})
+  })
+  });
+
+
+  router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    Posts.remove(id)
+      .then(post => {
+        res.status(200).json(post);
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ message: "Unable to delete post" });
+      });
+  });
+
+  router.put("/:id",  (req, res) => {
+    const changes = req.body;
+    const id = req.params.id;
+    console.log(req.body)
+    Posts.update(id, changes)
+      .then(post => {
+        res.status(200).json(post);
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ message: "Unable to update" });
+      });
+  });
 
 
 
